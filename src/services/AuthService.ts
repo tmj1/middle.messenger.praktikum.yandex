f7ae3b59bf7c60aa086bf2c07522083b4a8c1f00
tmj1 <tmj1@mail.ru>
2022-12-20 11:51:37 +0200

    Update AuthService.ts

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,4 +1,4 @@
-import { authAPI } from '../Api';
+import { authAPI } from '../Api/AuthAPI';
 import { SignupType, SigninType } from 'types';
 import { BrowseRouter as router, store } from 'core';
 import {
